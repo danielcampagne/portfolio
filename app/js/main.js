da9afad96b7e85d882831535526ce71de67856ec
Daniel <dcampagne@gmail.com>
2018-03-14 18:49:47 +0100

    Blotter working

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 
 	 //////// Bottler 
 
-	var text = new Blotter.Text("Daniel Campagne \u2013 Digital Designer", {
+	var header = new Blotter.Text("Daniel Campagne \u2013 Digital Designer", {
 		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
 		weight: "bold",
 		size : 70,
@@ -14,17 +14,93 @@ $( document ).ready(function() {
 		paddingRight: 100,
 	});
 
+	var kirsi = new Blotter.Text("Kirsi Sabri", {
+		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+		weight: "bold",
+		size : 90,
+		fill : "#000",
+		paddingTop: 100,
+		paddingBottom: 100,
+		paddingLeft: 100,
+		paddingRight: 100,
+	});
+
+	var moj = new Blotter.Text("My Own Jupiter", {
+		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+		weight: "bold",
+		size : 80,
+		fill : "#000",
+		paddingTop: 100,
+		paddingBottom: 100,
+		paddingLeft: 100,
+		paddingRight: 100,
+	});
+
+	var junait = new Blotter.Text("Junait", {
+		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+		weight: "bold",
+		size : 90,
+		fill : "#000",
+		paddingTop: 100,
+		paddingBottom: 100,
+		paddingLeft: 100,
+		paddingRight: 100,
+	});
+
+	var confapp = new Blotter.Text("ConfApp", {
+		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+		weight: "bold",
+		size : 90,
+		fill : "#000",
+		paddingTop: 100,
+		paddingBottom: 100,
+		paddingLeft: 100,
+		paddingRight: 100,
+	});
+
+	var jerszy = new Blotter.Text("Jerszy Seymour", {
+		family : "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+		weight: "bold",
+		size : 80,
+		fill : "#000",
+		paddingTop: 100,
+		paddingBottom: 100,
+		paddingLeft: 100,
+		paddingRight: 100,
+	});
+
 	var material = new Blotter.ChannelSplitMaterial();
 	material.uniforms.uRotation.value = 140;
-	var blotter = new Blotter(material, { texts : text });
-	var scope = blotter.forText(text);
+	var blotter = new Blotter(material, { 
+		texts : [header, kirsi, moj, junait, confapp, jerszy],
+		autobuild : false
+	});
+
+	blotter.needsUpdate = true;
+
+	var scope = blotter.forText(header);
+
 	scope.appendTo($(".title"));
+
 
 	///// Project on viewport
 
-	var $sections = $('.project');
+	var lastProject, currentProject;;
+	var $sections = $('.wrapper-projects section');
+
 	function checkVisibility() {
-	  $sections.removeClass('most-visible').mostVisible({percentage: false}).addClass('most-visible');
+		lastProject = $('.most-visible').attr('id');
+		currentProject = $sections.mostVisible().attr('id');
+		if (currentProject !== lastProject) {
+			$sections.removeClass('most-visible').mostVisible().addClass('most-visible');
+			
+			/// Change title
+			var scope = blotter.forText(eval(currentProject));
+			$(".title").html("");
+			scope.appendTo($(".title"));
+			console.log(currentProject);
+		}
+
 	}
 
 	///// Percentage of visible project
@@ -57,22 +133,17 @@ $( document ).ready(function() {
 	///// Add transitions
 
 	function setOpacity(value) {
-	var valueProcessed;
-	if (value <= 0.75) {
-		valueProcessed = (value - 0.75) * 4 + 1;
-	} else {
-		valueProcessed = 1;
-	}
-	console.log('value valueProcessed',Math.abs(valueProcessed));
-	//$('#text').css('opacity',valueProcessed);
-	material.uniforms.uOffset.value = 1 - valueProcessed;
+		var valueProcessed;
+		if (value <= 0.75) {
+			valueProcessed = (value - 0.75) * 4 + 1;
+		} else {
+			valueProcessed = 1;
+		}
+		//console.log('value valueProcessed',Math.abs(valueProcessed));
+		//$('#text').css('opacity',valueProcessed);
+		material.uniforms.uOffset.value = 1 - valueProcessed;
 	}
 
-	///// Change title
-
-	function toggleTittle() {
-
-	}
 
 	///// Listener
 
@@ -80,7 +151,7 @@ $( document ).ready(function() {
 
 	$(window).on('scroll', function () {
 		checkVisibility();
-		console.log($('.most-visible').ratioVisible());
+		//console.log($('.most-visible').ratioVisible());
 		setOpacity($('.most-visible').ratioVisible());
 	});
 
