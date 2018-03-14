93d8a0d0ccd1d680fe911280592dec8774074a36
Daniel <dcampagne@gmail.com>
2018-03-15 00:59:43 +0100

    added transitions on scroll

--- app/js/main.js
+++ app/js/main.js
@@ -139,6 +139,9 @@ $( document ).ready(function() {
 		if (value <= 0.75) {
 			valueProcessed = (value - 0.75) * 4 + 1;
 			console.log(valueProcessed);
+		// } else if (value >= 0.25) {
+		// 	valueProcessed = (value + 0.25) * 4 - 1;
+		// 	console.log('ex---', valueProcessed);
 		} else {
 			valueProcessed = 1;
 		}
@@ -158,9 +161,47 @@ $( document ).ready(function() {
 		setOpacity($('.most-visible').ratioVisible());
 	});
 
+	///// Reval elements on scroll
+
+	var rafId = null;
+	var delay = 200;
+	var lTime = 0;
+
+	function scroll() {
+	  var scrollTop = $(window).scrollTop();
+	  var height = $(window).height()
+	  var visibleTop = scrollTop + height;
+	  $('.reveal').each(function() {
+	    var $t = $(this);
+	    if ($t.hasClass('reveal_visible')) { return; }
+	    var top = $t.offset().top;
+	    if (top <= visibleTop) {
+	      if (top + $t.height() < scrollTop) {
+	        $t.removeClass('reveal_pending').addClass('reveal_visible');
+	      } else {
+	        $t.addClass('reveal_pending');
+	        if (!rafId) requestAnimationFrame(reveal);  
+	      }
+	    }
+	  });
+	}
+	function reveal() {
+	  rafId = null;
+	  var now = performance.now();
+	  
+	  if (now - lTime > delay) {
+	    lTime = now;
+	    var $ts = $('.reveal_pending');
+	    $($ts.get(0)).removeClass('reveal_pending').addClass('reveal_visible');  
+	  }
+	  
+	  
+	  if ($('.reveal_pending').length >= 1) rafId = requestAnimationFrame(reveal);
+	}
+
+	$(scroll);
+	$(window).scroll(scroll);
+
 });
 
-$( window ).on( "load", function() {
 
-
-})
