f84bec80bed948f57ca38425ae152b42fb87bddc
Daniel <dcampagne@gmail.com>
2018-04-24 06:33:16 +0200

    fixes on performances, added arrows

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,8 @@
 
 var screenFactor = 1.6;
 var isLoaded = false;
-/// Preload
+
+// Preload
 $( window ).on( "load", function(){
 	isLoaded = true
     $('.preloader').hide();
@@ -19,125 +20,68 @@ if ($(window).innerWidth() < 700) {
 		screenFactor = 1.4;
 	}
 }
-
 checkPosition();
 
-
 // Check if it's a retina display
-
 if (window.devicePixelRatio > 1) {
 	var isRetina = true;
 } 
 
 // Check if it's a mobile browser
-
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 	isMobile = true;
 }
 
-
+// When the DOM is loaded...
 $( document ).ready(function() {
-	//////// Projects Oject * Remember to duplicate the information here.
-		var projects = {	
-			"header" : {
-				text :"Daniel Campagne — Digital Designer", 
-				size: Math.round(50 * screenFactor)
-			},
-			"kirsi" : {
-				text :"Kirsi Sabri", 
-				size: Math.round(90 * screenFactor)
-			},
-			"moj" : {
-				text :"My Own Jupiter",
-				size: Math.round(80 * screenFactor)
-			},
-			"junait" : {
-				text :"Junait",
-				size: Math.round(90 * screenFactor)
-			},
-			"confapp" : {
-				text :"ConfApp",
-				size: Math.round(90 * screenFactor)
-			},
-			"jerszy" : {
-				text :"Jerszy Seymour",
-				size: Math.round(80 * screenFactor)
-			}
+	// Set project titles
+	var projects = {	
+		"header" : {
+			text :"Daniel Campagne — Digital Designer", 
+			size: Math.round(50 * screenFactor)
+		},
+		"kirsi" : {
+			text :"Kirsi Sabri", 
+			size: Math.round(95 * screenFactor)
+		},
+		"moj" : {
+			text :"My Own Jupiter",
+			size: Math.round(80 * screenFactor)
+		},
+		"junait" : {
+			text :"Junait",
+			size: Math.round(95 * screenFactor)
+		},
+		"confapp" : {
+			text :"ConfApp",
+			size: Math.round(95 * screenFactor)
+		},
+		"jerszy" : {
+			text :"Jerszy Seymour",
+			size: Math.round(80 * screenFactor)
+		}
+	};
 
-		};
-  
-
-	///// Generate index
-	var projectsIndex = $(".project").toArray();
-
-	for (var i = 0; i < projectsIndex.length; i++) {
-		$(".project__nav").append('<a class="project__nav__link project__nav__link--' + projectsIndex[i].id + ' " href="#' + projectsIndex[i].id +'" ><span class="project__nav__line"></span><span class="project__nav__name">' + projects[projectsIndex[i].id].text + '</span></a>');
-	}
-	$('.project__nav__link').on('click', function(event){     
-	    event.preventDefault();
-	    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800, 'swing');
-	});
-	$('.scroll-down').on('click', function(event){     
-	    event.preventDefault();
-	    $('html,body').animate({scrollTop:$('.project').offset().top}, 800, 'swing');
-	});
-
-
-
-
-	///// Project on viewport
-	var $sections = $('.wrapper--projects section');
-
+	// Check project on viewport
+	var sections = $('.wrapper--projects section');
+	var currentProject, lastProject;
 	function markVisible() {
 		lastProject = $('.most-visible').attr('id');
-		currentProject = $sections.mostVisible().attr('id');
+		currentProject = sections.mostVisible().attr('id');
+		console.log(currentProject);
 		if (currentProject !== lastProject) {
-			if (currentProject === 'header') {
-				$('.project__nav').fadeOut('slow');
-			} else {
-				if (lastProject === 'header') {
-					$('.project__nav').fadeIn('slow');
-				}
-			}
-			$sections.removeClass('most-visible').mostVisible().addClass('most-visible');		
-			$('.project__nav__link').removeClass('active');
-			$('.project__nav__link--' + currentProject + '').addClass('active');
+			sections.removeClass('most-visible').mostVisible().addClass('most-visible');
 			setCurrentTitle();
-			console.log(currentProject);
 		}
-
 	}
+	markVisible();
 
 	///// Set Current Title
-	var lastProject, currentProject, scopeMobile;
+	var scopeMobile;
 	function setCurrentTitle() {
 		scopeMobile = projects[currentProject].text;
 		$(".title--alternative").html(scopeMobile).css("font-size",projects[currentProject].size);
 	}
-	//// Define Transformation on scroll
-	function setTransformation(value) {
-		var valueProcessed;
-		if  (value <= 0.80) {
-			valueProcessed = (value - 0.80) * 5 + 1;
-		} else {
-			valueProcessed = 1;
-		}
-		//$('.project__description').css('opacity',valueProcessed);
-	}
-
-
-	///// Listener
-
-	markVisible();
-	document.addEventListener('scroll', (evt) => {
-		markVisible();
-		setTransformation($('.most-visible').ratioVisible());
-	}, {
-	  capture: true,
-	  passive: true
-	});
-
-
 
 	///// Snow overlay
 	$('.overlay__toggle').click(function(){
@@ -152,6 +96,11 @@ $( document ).ready(function() {
 		console.log($(this).parent().parent().parent().children('.level--top'));
 	});
 
-});
+	//// Listener
+	document.addEventListener('scroll', (evt) => {
+		markVisible();
+	}, { capture: true, passive: true });
 
+
+});
 
